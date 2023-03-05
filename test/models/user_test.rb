6ee1152abf7353f4c3a83cf9d74a437d81ef6242
Ilnur Nurumov <ilnur.nurumov@mai.ru>
2023-03-05 02:08:52 -0500

    brushed gemfile

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,4 +61,14 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+end
+
+  test "password should have minimum lenght" do
+    @user.password = @user.password_confirmation = " " * 5
+    assert_not @user.valid?
+end
 end
